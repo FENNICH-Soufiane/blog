854ddf8daf05250782445424519cbdf622dda8a6
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-07-19 20:46:46 +0100

    middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -6,21 +6,31 @@ import authRoutes from './routes/auth.route.js';
 
 const app = express();
 dotenv.config();
+app.use(express.json());
 
 mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
+.connect(process.env.MONGO)
+.then(() => {
         console.log('MongoDb is connected');
     })
     .catch((err) => {
         console.log(err);
     });
+    
+
+app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 
 app.listen(3000, () => {
     console.log('Server is runing on port 3000!')
 })
-
-
-app.use(express.json());
-
-app.use('/api/auth', authRoutes);
