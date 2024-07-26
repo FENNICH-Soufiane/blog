c2dd02f106317e83adda1577f70e62307c9926bb
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-07-26 22:44:20 +0100

    add delete user functionality to the admin user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
+
 
 const App = () => {
   return (
