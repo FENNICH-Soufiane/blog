0c7a70162be77a4c2f4e1d32bf04feff0e1849ad
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-07-26 16:19:26 +0100

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
 import cors from 'cors';
 
 
@@ -25,6 +26,7 @@ mongoose
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
