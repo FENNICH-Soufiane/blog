8f91b1542aba45058b98b4797731606ab9e5aeb0
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-07-26 15:33:10 +0100

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -6,23 +6,27 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true,
     },
-    password:{
+    password: {
         type: String,
-        required:true,
+        required: true,
     },
-        profilePicture: {
+    profilePicture: {
         type: String,
         default:
-          'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-      },
+            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+},
     { timestamps: true }
-    
+
 );
 
 const User = mongoose.model('User', userSchema);
